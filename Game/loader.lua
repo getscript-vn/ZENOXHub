a21f050c15fc508a493aa52eb4232c7bfa9473f3
getscript-vn <huuthienp05@gmail.com>
2025-05-27 16:23:06 +0700

    Update loader.lua

--- Game/loader.lua
+++ Game/loader.lua
@@ -1,1 +1,26 @@
-
+local a = tostring(game.PlaceId)
+local b = {}
+if b[a] then
+    a = b[a]
+end
+local function c()
+    local d, e =
+        pcall(
+        function()
+            return game:HttpGet(
+                "https://raw.githubusercontent.com/getscript-vn/ZENOXHub/refs/heads/main/Game/Load/" .. a
+            )
+        end
+    )
+    if d and type(e) == "string" then
+        local f, g = loadstring(e)
+        if f then
+            pcall(f)
+        else
+            warn("Error Load Script: ", g)
+        end
+    else
+        warn("Error Get Script")
+    end
+end
+c()
